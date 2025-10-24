0c5a1470bf9868495c674fd6d4af2f9a8c2db6ce
Pedro <pedrolmoreira99@gmail.com>
2025-10-24 13:48:22 -0300

    chore: removing .gitignore files

--- moovin_mobile/lib/core/error/api_exception.dart
+++
@@ -1,9 +0,0 @@
-class ApiException implements Exception {
-  final String message;
-  final String? code;
-
-  const ApiException(this.message, {this.code});
-
-  @override
-  String toString() => 'ApiException: $message';
-}
