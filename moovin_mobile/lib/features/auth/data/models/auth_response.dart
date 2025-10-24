56779d04fcb87bc84a5949d3e74063cc899bf98e
Pedro <pedrolmoreira99@gmail.com>
2025-10-24 13:50:47 -0300

    Reverting commit

---
+++ moovin_mobile/lib/features/auth/data/models/auth_response.dart
@@ -0,0 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'auth_response.g.dart';
+
+@JsonSerializable()
+class AuthResponse {
+  final int? id;
+  final String email;
+  final String name;
+  final String username;
+  final String userType;  // 'Proprietario', 'Inquilino', 'Admin'
+  final DateTime created;
+  final bool isActive;
+  final bool isStaff;
+
+  const AuthResponse({
+    this.id,
+    required this.email,
+    required this.name,
+    required this.username,
+    required this.userType,
+    required this.created,
+    required this.isActive,
+    required this.isStaff,
+  });
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthResponseToJson(this);
+}
