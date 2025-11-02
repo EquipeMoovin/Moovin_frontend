a0f50c8a26301133eabe1c2782272c88ebc92862
Pedro <pedrolmoreira99@gmail.com>
2025-11-02 19:36:41 -0300

    feat: Adicionar suporte a animações e rotas para o componente de visitas

--- moovin_workspace/apps/moovin_web/src/app/app.config.ts
+++ moovin_workspace/apps/moovin_web/src/app/app.config.ts
@@ -6,13 +6,18 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { HTTP_INTERCEPTORS,provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AuthInterceptor } from './services/auth/auth.interceptor';
 import { providePrimeNG } from 'primeng/config';
 import  Preset  from '../theme/preset';
 export const appConfig: ApplicationConfig = {
   providers: [
+    
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideBrowserGlobalErrorListeners(),
+    provideAnimations(),
+    provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(withInterceptorsFromDi()),
--- moovin_workspace/apps/moovin_web/src/app/app.routes.ts
+++ moovin_workspace/apps/moovin_web/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Route } from '@angular/router';
 import { Login } from './components/auth/login/login';
 import { Managment } from './components/managment/managment';
+import { Visits } from './components/managment/visits/visits';
+
 
 export const appRoutes: Route[] = [
     { path: 'login', component: Login },
     { path: 'register', component: Login }, // Temporário - substituir pelo componente de registro
-    { path: 'managment',component: Managment}
-     
+    { path: 'managment',component: Managment},
+    { path: 'add-visit', component: Visits },
 ];
